0cd828f132d59e8b5904b4f5518b872566f0b8d8
Fawad Ali <becomebetter101@gmail.com>
2025-05-22 21:53:05 +0500

    Fixed allowedHeaders

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
       ],
       methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', "OPTIONS"],
       credentials: true,
-      allowedHeaders: "Access-Control-Allow-Origin"
+      allowedHeaders: ["Content-Type", "Authorization"]
     }
   });
 
